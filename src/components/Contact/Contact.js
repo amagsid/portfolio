4428c4bafa09d81cff0869f8e14d244d37f5838c
Ahmad <amagsid@gmail.com>
2023-07-06 09:28:15 +0200

    worked on send button

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { BiMailSend } from 'react-icons/bi';
-
 import { init, sendForm } from 'emailjs-com';
 init('user_x4FGTrgTjdeEVdrn9HdcN');
 import { Section, SectionText } from '../../styles/GlobalComponents';
 import MovingSectionTitle from '../SectionTitle/SectionTitle';
-
-import { Form, Container } from 'react-bootstrap';
-
-import { InputField } from './ContactStyles';
+import { Form, Container, Col, Row } from 'react-bootstrap';
+import { Input, CharAlert, CharCount, Placeholder } from './ContactStyles';
+import Button from '../../elements/SendButton/Button';
 
 function Contact() {
 	const {
@@ -45,66 +43,69 @@ function Contact() {
 	};
 
 	return (
-		<Section id="about">
+		<Section id="contact">
 			<MovingSectionTitle sectionTitle="GET IN TOUCH"></MovingSectionTitle>
-			<Container fluid>
+			<Container style={{ height: 220 }} fluid>
 				{emailSent && <p>🚀 Your message was sent , I'll get back to you shortly!</p>}
 				<Form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
 					<input type="hidden" name="contact_number" value={contactNumber} />
 
-					{/* name */}
-					<Form.Group className="mb-1" controlId="formBasicText">
-						<Form.Control
-							className="mb-2 input-field"
-							size="lg"
-							type="text"
-							name="user_name"
-							placeholder="name"
-							{...register('user_name')}
-							maxLength="30"
-							aria-invalid={errors.user_name ? 'true' : 'false'}
-							required={true}
-						/>
-					</Form.Group>
+					<Row>
+						<Col style={{ paddingLeft: 0, paddingRight: 2 }} className="mb-2">
+							{/* name */}
 
-					{/* email */}
-					<Form.Group controlId="exampleForm.ControlInput1">
-						<Form.Control
-							style={{
-								backgroundColor: 'rgb(15,22,90,0)',
-								color: 'white',
-							}}
-							size="lg"
-							type="email"
-							name="user_email"
-							placeholder="email@example.com"
-							{...register('user_email')}
-						></Form.Control>
-					</Form.Group>
-					{/* message */}
-					<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-						<Form.Control
-							style={{
-								backgroundColor: 'rgb(15,22,90,0)',
-								color: 'white',
-							}}
-							size="lg"
-							name="enter your message"
-							{...register('message')}
-							maxLength="1500"
-							placeholder="Message"
-							as="textarea"
-							rows={6}
-						/>
-					</Form.Group>
+							<Form.Group controlId="formBasicText">
+								<Input
+									// size="lg"
+									type="text"
+									name="user_name"
+									placeholder="name"
+									{...register('user_name')}
+									maxLength="30"
+									aria-invalid={errors.user_name ? 'true' : 'false'}
+									required={true}
+								/>
+							</Form.Group>
+						</Col>
 
-					{/* charachter count */}
-					<p className="message-chars-left">{messageCharsLeft} characeters left</p>
-					<br />
-					{/* send button */}
-					<button className="pulse send-icon" type="submit" value="Send">
-						Send <BiMailSend className="send-icon" size={20} />
-					</button>
+						<Col style={{ paddingRight: 0, paddingLeft: 2 }}>
+							{/* email */}
+							<Form.Group controlId="exampleForm.ControlInput1">
+								<Input
+									type="email"
+									name="user_email"
+									placeholder="email@example.com"
+									{...register('user_email')}
+									required={true}
+								/>
+							</Form.Group>
+						</Col>
+					</Row>
+
+					<Row>
+						{/* message */}
+						<Form.Group className=" p-0" controlId="exampleForm.ControlTextarea1">
+							<Input
+								name="enter your message"
+								{...register('message')}
+								maxLength="1500"
+								placeholder="Message"
+								as="textarea"
+								rows={3}
+								required={true}
+							/>
+							{/* charachter count */}
+							<CharAlert className="message-chars-left">
+								<CharCount>{messageCharsLeft} </CharCount> characeters left
+							</CharAlert>
+							{/* send button */}
+							{/* <button className="form-button send-icon" type="submit" value="Send"> */}
+							{/* SEND */}
+							{/* <BiMailSend className="send-icon" size={20} /> */}
+							{/* </button> */}
+							<Button />
+						</Form.Group>
+					</Row>
 				</Form>
 			</Container>
 		</Section>
