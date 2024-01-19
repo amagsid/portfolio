1044ea52b9afd282d1883a4098fc8f4a4dd6cbd9
Ahmad <amagsid@gmail.com>
2024-01-19 09:16:03 +0100

    worked on perojects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,7 +17,7 @@ const PastWork = () => {
       name: 'Booking.com Top 5 Hotels',
       img: BDCRightInd,
       description:
-        'client needed a template to showcase the top 5 hotels for different cities around the world while accelerating their content creation process without the need of having to create a different video for 50+ cities. By creating this UI template that gets converted into a rendered video by filling entried in a Google sheet, client was able to demonstrate different accomodation options worldwide and their attributes on the spot',
+        'Modular  UI template showcasing top stays in cities around the world. By creating this UI template that gets rendered into a video, client was able to demonstrate different accomodation and their attributes worldwide on the spot without having to create a different video for 50+ cities.',
       link: '',
       techStack: ['Figma', 'JavaScript', 'CSS', 'Nuke'],
     },
@@ -25,7 +25,7 @@ const PastWork = () => {
     {
       name: 'Nike Landing Page Mockup',
       img: Nike,
-      description: `As I got more intersted in Tailwind and what it has to offer compafred to other styline options out there, after slef-studying Tailwind, I needed a side project to experiment but also expand my hands-on knowledge using it. I picked the Nike.com website with the goal to create a failthful replica. Throughout the process, styling wasn't my only focus but also responsivity`,
+      description: `As I got more intersted in Tailwind and what its robustness, I needed a side project to experiment but also expand my hands-on knowledge using it. I picked the Nike.com website with the goal to create a failthful replica. Throughout the process, styling wasn't my only focus but also responsivity`,
       hovered: false,
       link: '',
       techStack: ['Figma', 'Next.js', 'Tailwind'],
@@ -33,7 +33,7 @@ const PastWork = () => {
     {
       name: 'Bol.com 7 Daagse campaign',
       img: Bol,
-      description: `For their annual 7 Daagse campaign, Bol was looking for a UI template to accelerate the preparation for this campaign across Netherlands and Belgium. The material ended up going live on different mediums and the challenge was to create the template that is flexible based on the differen characteristics of the different platforms`,
+      description: `For their annual 7 Daagse campaign, Bol was needed a solution to accelerate the preparation for the campaign across The Netherlands and Belgium. The challenge was to create the template with flexibility for it to go live omn different mediums, including TV.`,
       hovered: false,
       link: 'https://www.linkedin.com/posts/cube-cloud_cube-x-dept-x-bolcom-bol-7-daagse-is-back-activity-7047152373227536384-rb-n?utm_source=share&utm_medium=member_desktop',
       techStack: ['Figma', 'JavaScript', 'CSS'],
@@ -43,7 +43,7 @@ const PastWork = () => {
       name: 'SquareSpace',
       img: SqSp,
       hovered: false,
-      description: `The goal of this UI template was to showcase how SquareSpace can provide different and interchangeable themes for all kinds of businesses. for that, they used a template that focused on typography and interchangeable images toshowcase real business that used SquareSpace for their website creation`,
+      description: `Client wanted to sbowcase the ability of providing customizable website templates. They used a template that focused on typography and interchangeable images to showcase business that used SquareSpace for their website.`,
       link: '',
       techStack: ['Figma', 'JavaScript', 'CSS'],
     },
